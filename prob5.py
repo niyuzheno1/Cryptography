9144a490c521896f8dc7fe07dff4f613ed67de5d
Zack Ni <yn117@scarletmail.rutgers.edu>
2021-01-15 22:11:07 -0800

    Update prob5.py

--- prob5.py
+++ prob5.py
@@ -41,19 +41,24 @@ def A(x):
 def B(k, delta, y):
 	global m, n
 	F = GetF(k,f)
+	# question 1: how the probability will be for each episodes
 	for i in range(0, int((k**2) * n/ delta)):
-		x  = np.random.randint(2, size=n * k)
-		x = x.reshape((k,n))
-		y_bar= F(x)
-		y[i] = y_bar[i]
-		x_prime = np.asarray(A(y)).reshape((k,n))
-		if F(x_prime) == y:
-			return x_prime
+		x  = np.random.randint(2, size=n * k) # part of D_y
+		x = x.reshape((k,n)) 
+		y_bar= F(x) 
+		y_bar = np.asarray(y_bar)
 		
-	
+		y_bar = y_bar.reshape((k,m))
+		some_i = random.randint(0, y_bar.shape[0]-1) # part of D_y
+		y_bar[some_i] = np.asarray(y)
+		y_bar = y_bar.flatten()
+		y_bar = list(y_bar)
+		x_prime = np.asarray(A(y_bar)).reshape((k,n))
+		if F(x_prime) == y_bar:
+			return x_prime[some_i]
 	print("failed")
 
 
 init()
 
-print(B(2, 1, [0,0,0,0,0,0,0,0]))
+print(B(2, 1, [0,0,0,0]))
